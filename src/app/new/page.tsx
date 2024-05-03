1dccedd47d3a4947bf5b078088252b29196fe9e0
Zsigmond <e0725064@u.nus.edu>
2024-05-03 18:11:57 +0800

    feat: generate new puzzles at /new

---
+++ src/app/new/page.tsx
@@ -0,0 +1,12 @@
+import { getSudoku } from 'sudoku-gen';
+import GridClient from '../[puzzle_id]/components/gridClient';
+
+export default async function RandomNew() {
+  const puzzle = getSudoku();
+
+  return (
+    <main className="flex h-screen flex-col items-center justify-between p-2">
+      <GridClient puzzle={puzzle.puzzle} />
+    </main>
+  );
+}
