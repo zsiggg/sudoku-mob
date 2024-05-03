8be6c3f956fd36cde1c7a7220e2050f58551fce2
Zsigmond <e0725064@u.nus.edu>
2024-05-04 03:42:50 +0800

    refactor: grid component in puzzleClient

---
+++ src/app/components/puzzleClient/grid.tsx
@@ -0,0 +1,175 @@
+import {
+  getRowColBoxIdxs,
+  updateDigits,
+  updateValidDigits,
+} from '@/app/utils/puzzleClient/helper';
+import { Dispatch, FormEvent, SetStateAction, useState } from 'react';
+
+const Grid = ({
+  puzzleId,
+  puzzleRowNum,
+  initialDigits,
+  digits,
+  setDigits,
+  emptyCellCount,
+  setEmptyCellCount,
+}: {
+  puzzleId?: string;
+  puzzleRowNum?: number;
+  initialDigits: string[];
+  digits: string[];
+  setDigits: Dispatch<SetStateAction<string[]>>;
+  emptyCellCount: number;
+  setEmptyCellCount: Dispatch<SetStateAction<number>>;
+}) => {
+  const initialIsHighlightedArr = Array.from({ length: 81 }, () => false);
+  const [isHighlightedArr, setIsHighlightedArr] = useState(
+    initialIsHighlightedArr,
+  );
+  const [hoveredIdx, setHoveredIdx] = useState<number | null>(null);
+  const [clickedIdx, setClickedIdx] = useState<number | null>(null);
+
+  // assume puzzle is valid (no duplicate digits in rows, cols, or boxes)
+  const [validDigits, setValidDigits] = useState(
+    Array.from({ length: digits.length }, () => true),
+  );
+
+  const onDigitInput = (i: number, e: FormEvent<HTMLInputElement>) => {
+    const digit = parseInt(e.currentTarget.value);
+    const prevDigit = digits[i];
+
+    if (e.currentTarget.value == '') {
+      // store empty string if user backspaces
+      const newDigits = updateDigits(i, '', digits, setDigits);
+      updateValidDigits(
+        i,
+        '',
+        prevDigit,
+        validDigits,
+        setValidDigits,
+        newDigits,
+      );
+
+      setEmptyCellCount(emptyCellCount + 1);
+    } else if (e.currentTarget.value.length == 1 && digit >= 1 && digit <= 9) {
+      // store valid digit
+      const newDigits = updateDigits(i, digit.toString(), digits, setDigits);
+      updateValidDigits(
+        i,
+        digit.toString(),
+        prevDigit,
+        validDigits,
+        setValidDigits,
+        newDigits,
+      );
+
+      // if new digit was input
+      if (prevDigit === '') {
+        setEmptyCellCount(emptyCellCount - 1);
+      }
+    } else {
+      // don't change input if other characters are input
+      e.currentTarget.value = digits[i];
+    }
+  };
+
+  const highlightRowColOfCell = (i: number) => {
+    const rowColBoxIdxs = getRowColBoxIdxs(i);
+    const toBeHighlightedIdxs = rowColBoxIdxs.row.concat(rowColBoxIdxs.col);
+    const toBeHighlightedIdxsSet = new Set(toBeHighlightedIdxs);
+    const newIsHighlightedArr = isHighlightedArr.map((_, i) =>
+      toBeHighlightedIdxsSet.has(i) ? true : false,
+    );
+    setIsHighlightedArr(newIsHighlightedArr);
+  };
+
+  const unhighlightAllCells = () => {
+    setIsHighlightedArr(initialIsHighlightedArr);
+  };
+  const onHover = (i: number) => {
+    setHoveredIdx(i);
+    if (clickedIdx === null) {
+      highlightRowColOfCell(i);
+    }
+  };
+
+  const onNoHover = () => {
+    setHoveredIdx(null);
+    if (clickedIdx === null) {
+      unhighlightAllCells();
+    }
+  };
+
+  const onFocus = (i: number) => {
+    setClickedIdx(i);
+    highlightRowColOfCell(i);
+  };
+
+  const onBlur = () => {
+    setClickedIdx(null);
+    if (hoveredIdx === null) {
+      unhighlightAllCells();
+    } else {
+      highlightRowColOfCell(hoveredIdx);
+    }
+  };
+
+  return (
+    <>
+      <div className="text-center text-3xl text-sky-800">
+        {puzzleId === undefined ? (
+          <p>New Puzzle</p>
+        ) : puzzleRowNum === undefined ? (
+          <div className="ml-1 h-10 w-44 animate-pulse rounded-lg bg-sky-100"></div>
+        ) : (
+          <p>{`Puzzle ${puzzleRowNum}`}</p>
+        )}
+      </div>
+      <div
+        className="grid aspect-square w-full grid-cols-9 grid-rows-9 border-2 border-sky-950 lg:w-2/5 xl:w-1/2"
+        onMouseLeave={onNoHover}
+      >
+        {initialDigits.map((digit, i) => {
+          // booleans to determine grid cells' borders
+          const isLastRow = i + 1 >= 72;
+          const isLastCol = (i + 1) % 9 == 0;
+          const isMultipleOf3InnerRow = i % 27 >= 18 && !isLastRow;
+          const isMultipleof3InnerCol = (i + 1) % 3 == 0 && !isLastCol;
+          const borderClasses = `border-[0.5px] border-sky-800 border-opacity-60 ${
+            isMultipleOf3InnerRow ? 'border-b-[2px]' : ''
+          } ${isMultipleof3InnerCol ? 'border-r-[2px]' : ''}`;
+          const isDigitFromPuzzle = !isNaN(parseInt(digit));
+
+          return isDigitFromPuzzle ? (
+            <div
+              key={i}
+              tabIndex={i}
+              className={`flex size-full items-center justify-center ${borderClasses} ${validDigits[i] ? 'text-sky-800/50' : 'text-red-600/50'} ${clickedIdx === i ? 'bg-sky-800/50' : isHighlightedArr[i] ? 'bg-sky-800/25' : ''}`}
+              onMouseEnter={() => onHover(i)}
+              onFocus={() => onFocus(i)}
+              onBlur={onBlur}
+            >
+              {digits[i]}
+            </div>
+          ) : (
+            <input
+              type="text"
+              key={i}
+              tabIndex={i}
+              max={9}
+              min={0}
+              step={1}
+              className={`flex items-center justify-center ${borderClasses} bg-inherit text-center ${validDigits[i] ? '' : 'text-red-600'} ${clickedIdx === i ? 'bg-sky-800/50' : isHighlightedArr[i] ? 'bg-sky-800/25' : ''}`}
+              onInput={(e) => onDigitInput(i, e)}
+              onMouseEnter={() => onHover(i)}
+              onFocus={() => onFocus(i)}
+              onBlur={onBlur}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Grid;
--- src/app/components/puzzleClient/puzzleClient.tsx
+++ src/app/components/puzzleClient/puzzleClient.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
-import {
-  getRowColBoxIdxs,
-  isValidSudoku,
-  updateDigits,
-  updateValidDigits,
-} from '../../utils/puzzleClient/helper';
+import { useState } from 'react';
+import { isValidSudoku } from '../../utils/puzzleClient/helper';
 import SubmissionToast from './submissionToast';
 import { addPuzzle, checkIsPuzzleInDb } from '../../utils/supabase/puzzlesDb';
 import ControlRow from './controlRow';
+import Grid from './grid';
 
 const PuzzleClient = ({
   puzzle,
@@ -26,13 +22,6 @@ const PuzzleClient = ({
   const [showFailureToast, setShowFailureToast] = useState(false);
   const [showAddedToDbToast, setShowAddedToDbToast] = useState(false);
 
-  const initialIsHighlightedArr = Array.from({ length: 81 }, () => false);
-  const [isHighlightedArr, setIsHighlightedArr] = useState(
-    initialIsHighlightedArr,
-  );
-  const [hoveredIdx, setHoveredIdx] = useState<number | null>(null);
-  const [clickedIdx, setClickedIdx] = useState<number | null>(null);
-
   const initialDigits = puzzle.split('').map((str) => (str === '.' ? '' : str));
   const initialEmptyCellCount = initialDigits.reduce(
     (count, digit) => (digit === '' ? count + 1 : count),
@@ -40,49 +29,6 @@ const PuzzleClient = ({
   );
   const [emptyCellCount, setEmptyCellCount] = useState(initialEmptyCellCount);
   const [digits, setDigits] = useState(initialDigits);
-  // assume puzzle is valid (no duplicate digits in rows, cols, or boxes)
-  const [validDigits, setValidDigits] = useState(
-    Array.from({ length: digits.length }, () => true),
-  );
-
-  const onDigitInput = (i: number, e: FormEvent<HTMLInputElement>) => {
-    const digit = parseInt(e.currentTarget.value);
-    const prevDigit = digits[i];
-
-    if (e.currentTarget.value == '') {
-      // store empty string if user backspaces
-      const newDigits = updateDigits(i, '', digits, setDigits);
-      updateValidDigits(
-        i,
-        '',
-        prevDigit,
-        validDigits,
-        setValidDigits,
-        newDigits,
-      );
-
-      setEmptyCellCount(emptyCellCount + 1);
-    } else if (e.currentTarget.value.length == 1 && digit >= 1 && digit <= 9) {
-      // store valid digit
-      const newDigits = updateDigits(i, digit.toString(), digits, setDigits);
-      updateValidDigits(
-        i,
-        digit.toString(),
-        prevDigit,
-        validDigits,
-        setValidDigits,
-        newDigits,
-      );
-
-      // if new digit was input
-      if (prevDigit === '') {
-        setEmptyCellCount(emptyCellCount - 1);
-      }
-    } else {
-      // don't change input if other characters are input
-      e.currentTarget.value = digits[i];
-    }
-  };
 
   const onSubmit = async () => {
     const isValid = isValidSudoku(digits);
@@ -103,48 +49,6 @@ const PuzzleClient = ({
     }
   };
 
-  const highlightRowColOfCell = (i: number) => {
-    const rowColBoxIdxs = getRowColBoxIdxs(i);
-    const toBeHighlightedIdxs = rowColBoxIdxs.row.concat(rowColBoxIdxs.col);
-    const toBeHighlightedIdxsSet = new Set(toBeHighlightedIdxs);
-    const newIsHighlightedArr = isHighlightedArr.map((_, i) =>
-      toBeHighlightedIdxsSet.has(i) ? true : false,
-    );
-    setIsHighlightedArr(newIsHighlightedArr);
-  };
-
-  const unhighlightAllCells = () => {
-    setIsHighlightedArr(initialIsHighlightedArr);
-  };
-
-  const onHover = (i: number) => {
-    setHoveredIdx(i);
-    if (clickedIdx === null) {
-      highlightRowColOfCell(i);
-    }
-  };
-
-  const onNoHover = () => {
-    setHoveredIdx(null);
-    if (clickedIdx === null) {
-      unhighlightAllCells();
-    }
-  };
-
-  const onFocus = (i: number) => {
-    setClickedIdx(i);
-    highlightRowColOfCell(i);
-  };
-
-  const onBlur = () => {
-    setClickedIdx(null);
-    if (hoveredIdx === null) {
-      unhighlightAllCells();
-    } else {
-      highlightRowColOfCell(hoveredIdx);
-    }
-  };
-
   return (
     <>
       <SubmissionToast
@@ -156,58 +60,15 @@ const PuzzleClient = ({
         onAddedToDbDismiss={() => setShowAddedToDbToast(false)} // test
       />
       <div className="flex h-full flex-col items-center justify-center space-y-5 text-xl md:p-10 lg:space-y-7 xl:p-5">
-        <div className="text-center text-3xl text-sky-800">
-          {puzzleId === undefined ? (
-            <p>New Puzzle</p>
-          ) : puzzleRowNum === undefined ? (
-            <div className="ml-1 h-10 w-44 animate-pulse rounded-lg bg-sky-100"></div>
-          ) : (
-            <p>{`Puzzle ${puzzleRowNum}`}</p>
-          )}
-        </div>
-        <div
-          className="grid aspect-square w-full grid-cols-9 grid-rows-9 border-2 border-sky-950 lg:w-2/5 xl:w-1/2"
-          onMouseLeave={onNoHover}
-        >
-          {initialDigits.map((digit, i) => {
-            // booleans to determine grid cells' borders
-            const isLastRow = i + 1 >= 72;
-            const isLastCol = (i + 1) % 9 == 0;
-            const isMultipleOf3InnerRow = i % 27 >= 18 && !isLastRow;
-            const isMultipleof3InnerCol = (i + 1) % 3 == 0 && !isLastCol;
-            const borderClasses = `border-[0.5px] border-sky-800 border-opacity-60 ${
-              isMultipleOf3InnerRow ? 'border-b-[2px]' : ''
-            } ${isMultipleof3InnerCol ? 'border-r-[2px]' : ''}`;
-            const isDigitFromPuzzle = !isNaN(parseInt(digit));
-
-            return isDigitFromPuzzle ? (
-              <div
-                key={i}
-                tabIndex={i}
-                className={`flex size-full items-center justify-center ${borderClasses} ${validDigits[i] ? 'text-sky-800/50' : 'text-red-600/50'} ${clickedIdx === i ? 'bg-sky-800/50' : isHighlightedArr[i] ? 'bg-sky-800/25' : ''}`}
-                onMouseEnter={() => onHover(i)}
-                onFocus={() => onFocus(i)}
-                onBlur={onBlur}
-              >
-                {digits[i]}
-              </div>
-            ) : (
-              <input
-                type="text"
-                key={i}
-                tabIndex={i}
-                max={9}
-                min={0}
-                step={1}
-                className={`flex items-center justify-center ${borderClasses} bg-inherit text-center ${validDigits[i] ? '' : 'text-red-600'} ${clickedIdx === i ? 'bg-sky-800/50' : isHighlightedArr[i] ? 'bg-sky-800/25' : ''}`}
-                onInput={(e) => onDigitInput(i, e)}
-                onMouseEnter={() => onHover(i)}
-                onFocus={() => onFocus(i)}
-                onBlur={onBlur}
-              />
-            );
-          })}
-        </div>
+        <Grid
+          puzzleId={puzzleId}
+          puzzleRowNum={puzzleRowNum}
+          initialDigits={initialDigits}
+          digits={digits}
+          setDigits={setDigits}
+          emptyCellCount={emptyCellCount}
+          setEmptyCellCount={setEmptyCellCount}
+        />
         <ControlRow
           puzzleIds={puzzleIds}
           puzzleId={puzzleId}
