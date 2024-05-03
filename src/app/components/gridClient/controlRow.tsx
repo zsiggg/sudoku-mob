d4d90b892915fcbbe4f40f5755669da4066c4f07
Zsigmond <e0725064@u.nus.edu>
2024-05-04 03:18:17 +0800

    refactor: controlRow components

--- src/app/components/gridClient/controlRow.tsx
+++ src/app/components/gridClient/controlRow.tsx
@@ -1,7 +1,5 @@
-import { CheckIcon, PlusIcon } from '@heroicons/react/16/solid';
-import { Dropdown } from 'flowbite-react';
-import { revalidateRootPath } from '../../utils/helper';
-import Link from 'next/link';
+import PuzzleDropdownButton from './puzzleDropdownButton';
+import SubmitButton from './submitButton';
 
 const ControlRow = ({
   puzzleIds,
@@ -16,65 +14,8 @@ const ControlRow = ({
 }) => {
   return (
     <div className="flex items-center justify-center gap-5">
-      <Dropdown
-        className="max-h-96 overflow-y-auto"
-        placement="top"
-        label=""
-        renderTrigger={() => (
-          <button>
-            <PlusIcon className="size-12 text-sky-800 lg:size-10" />
-          </button>
-        )}
-      >
-        <Dropdown.Header className="p-4 font-bold">
-          Choose a new puzzle
-        </Dropdown.Header>
-        <Dropdown.Item
-          as={Link}
-          href={`/puzzle/new`}
-          onClick={() => revalidateRootPath()}
-          className="w-56 p-4 hover:bg-sky-100"
-        >
-          New Puzzle
-        </Dropdown.Item>
-        <Dropdown.Item
-          as={Link}
-          href={`/puzzle`}
-          onClick={() => revalidateRootPath()}
-          className="w-56 p-4 hover:bg-sky-100"
-        >
-          Random from database
-        </Dropdown.Item>
-        {puzzleIds.map((id, i) => (
-          <>
-            {id !== puzzleId ? (
-              <Dropdown.Item
-                as={Link}
-                href={`/puzzle/${id}`}
-                key={id}
-                className="w-56 p-4 hover:bg-sky-100"
-              >
-                Puzzle {i + 1}
-              </Dropdown.Item>
-            ) : (
-              <Dropdown.Item
-                as="button"
-                key={id}
-                disabled={true}
-                className="w-56 bg-gray-100 p-4 opacity-50"
-              >
-                Puzzle {i + 1}
-              </Dropdown.Item>
-            )}
-          </>
-        ))}
-      </Dropdown>
-
-      <button disabled={emptyCellCount !== 0} onClick={() => onSubmit()}>
-        <CheckIcon
-          className={`mx-auto size-12 lg:size-10 ${emptyCellCount === 0 ? 'text-sky-800/100' : 'text-sky-800/25'}`}
-        />
-      </button>
+      <PuzzleDropdownButton puzzleIds={puzzleIds} puzzleId={puzzleId} />
+      <SubmitButton emptyCellCount={emptyCellCount} onSubmit={onSubmit} />
     </div>
   );
 };
