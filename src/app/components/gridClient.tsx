7a5f614a8af9919703d50aecb6e914e1cdc48fad
Zsigmond <e0725064@u.nus.edu>
2024-05-03 19:22:20 +0800

    feat: show toast upon adding new puzzle to db

--- src/app/components/gridClient.tsx
+++ src/app/components/gridClient.tsx
@@ -23,6 +23,7 @@ const GridClient = ({
 }) => {
   const [showSuccessToast, setShowSuccessToast] = useState(false);
   const [showFailureToast, setShowFailureToast] = useState(false);
+  const [showAddedToDbToast, setShowAddedToDbToast] = useState(false);
 
   const initialIsHighlightedArr = Array.from({ length: 81 }, () => false);
   const [isHighlightedArr, setIsHighlightedArr] = useState(
@@ -85,11 +86,12 @@ const GridClient = ({
   const onSubmit = async () => {
     const isValid = isValidSudoku(digits);
     if (isValid) {
-      if (puzzle_id === undefined) {
-        await addPuzzle(puzzle);
-      }
       setShowFailureToast(false);
       setShowSuccessToast(true);
+      if (puzzle_id === undefined) {
+        await addPuzzle(puzzle);
+        setShowAddedToDbToast(true);
+      }
     } else {
       setShowFailureToast(true);
       setTimeout(() => setShowFailureToast(false), 5000);
@@ -175,8 +177,10 @@ const GridClient = ({
       <SubmissionToast
         isShowingSuccess={showSuccessToast}
         isShowingFailure={showFailureToast}
+        isShowingAddedToDb={showAddedToDbToast}
         onSuccessDismiss={() => setShowSuccessToast(false)}
         onFailureDismiss={() => setShowFailureToast(false)}
+        onAddedToDbDismiss={() => setShowAddedToDbToast(false)} // test
       />
       <div className="flex h-full flex-col items-center justify-center space-y-5 text-xl md:p-10 lg:space-y-7 xl:p-5">
         <div
--- src/app/components/submissionToast.tsx
+++ src/app/components/submissionToast.tsx
@@ -1,32 +1,58 @@
 import { Transition } from '@headlessui/react';
-import { CheckIcon, XMarkIcon } from '@heroicons/react/16/solid';
+import { BoltIcon, CheckIcon, XMarkIcon } from '@heroicons/react/16/solid';
 import { Toast } from 'flowbite-react';
 import Link from 'next/link';
 
 const SubmissionToast = ({
   isShowingSuccess,
   isShowingFailure,
+  isShowingAddedToDb,
   onSuccessDismiss,
   onFailureDismiss,
+  onAddedToDbDismiss,
 }: {
   isShowingSuccess: boolean;
   isShowingFailure: boolean;
+  isShowingAddedToDb?: boolean;
   onSuccessDismiss: () => void;
   onFailureDismiss: () => void;
+  onAddedToDbDismiss?: () => void;
 }) => {
   return (
     <>
       <Transition
         appear={true}
-        show={isShowingSuccess}
-        enter="transition-transform transition-gpu duration-300 ease-out"
+        show={!!isShowingAddedToDb}
+        enter="transition-transform transform-gpu duration-300 ease-out"
         enterFrom="-translate-y-20"
         enterTo="translate-y-0"
-        leave="transition-transform transition-gpu duration-300 ease-out"
+        leave="transition-transform transform-gpu duration-300 ease-out"
         leaveFrom="translate-y-0"
         leaveTo="-translate-y-20"
       >
-        <Toast className="fixed left-1/2 top-0 w-max -translate-x-1/2 translate-y-5 bg-white p-2">
+        <Toast
+          className={`fixed left-1/2 top-5 w-max -translate-x-1/2 ${isShowingSuccess || isShowingFailure ? 'translate-y-16' : 'translate-y-0'} transform-gpu bg-white p-2 transition-transform duration-300 ease-out`}
+        >
+          <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-yellow-100 text-yellow-500">
+            <BoltIcon className="size-5" />
+          </div>
+          <div className="ml-2 mr-1 text-sm">
+            Added new puzzle to the database!
+          </div>
+          <Toast.Toggle onDismiss={onAddedToDbDismiss} />
+        </Toast>
+      </Transition>
+      <Transition
+        appear={true}
+        show={isShowingSuccess}
+        enter="transition-transform transform-gpu duration-300 ease-out"
+        enterFrom="-translate-y-20"
+        enterTo="translate-y-0"
+        leave="transition-transform transform-gpu duration-300 ease-out"
+        leaveFrom="translate-y-0"
+        leaveTo="-translate-y-20"
+      >
+        <Toast className="fixed left-1/2 top-5 w-max -translate-x-1/2 bg-white p-2">
           <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500">
             <CheckIcon className="size-5" />
           </div>
@@ -43,14 +69,14 @@ const SubmissionToast = ({
       <Transition
         appear={true}
         show={isShowingFailure}
-        enter="transition-transform transition-gpu duration-300 ease-out"
+        enter="transition-transform transform-gpu duration-300 ease-out"
         enterFrom="-translate-y-20"
         enterTo="translate-y-0"
-        leave="transition-transform transition-gpu duration-300 ease-out"
+        leave="transition-transform transform-gpu duration-300 ease-out"
         leaveFrom="translate-y-0"
         leaveTo="-translate-y-20"
       >
-        <Toast className="fixed left-1/2 top-0 w-max -translate-x-1/2 translate-y-5 bg-white p-2">
+        <Toast className="fixed left-1/2 top-5 w-max -translate-x-1/2 bg-white p-2">
           <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500">
             <XMarkIcon className="size-5" />
           </div>
