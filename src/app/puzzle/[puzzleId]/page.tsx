1a7e7c86ea882145ff152a7ace368eca1feb1bab
Zsigmond <e0725064@u.nus.edu>
2024-05-05 04:28:57 +0800

    fix: grid getting pushed down when toasts appear

--- src/app/puzzle/[puzzleId]/page.tsx
+++ src/app/puzzle/[puzzleId]/page.tsx
@@ -5,7 +5,7 @@ import PuzzleLoading from './loading';
 export default function Home({ params }: { params: { puzzleId: string } }) {
   const { puzzleId } = params;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+    <main className="flex min-h-screen flex-col items-center justify-center p-2">
       <Suspense fallback={<PuzzleLoading />}>
         <PuzzleServer puzzleId={puzzleId} />
       </Suspense>
