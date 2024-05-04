e79003a696198318f3b136b0b22489c6210ef1b4
Zsigmond <e0725064@u.nus.edu>
2024-05-05 04:38:10 +0800

    feat: generate easy puzzles

--- src/app/puzzle/new/page.tsx
+++ src/app/puzzle/new/page.tsx
@@ -2,7 +2,7 @@ import PuzzleServer from '@/app/components/puzzleServer';
 import { getSudoku } from 'sudoku-gen';
 
 export default async function RandomNew() {
-  const puzzleObj = getSudoku();
+  const puzzleObj = getSudoku('easy');
   const puzzle = puzzleObj.puzzle.replace(/-/g, '.');
 
   return (
