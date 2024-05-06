fe68809505af1b5451c531c8395e9ea443e7656d
Zsigmond <e0725064@u.nus.edu>
2024-05-06 21:05:35 +0800

    fix: toast width too small before appearing

--- src/app/components/puzzleClient/submissionToast.tsx
+++ src/app/components/puzzleClient/submissionToast.tsx
@@ -42,9 +42,10 @@ const SubmissionToast = ({
             ? '-translate-y-36'
             : '-translate-y-20'
         }
+        className="fixed top-0 w-screen"
       >
         <Toast
-          className={`fixed left-1/2 top-5 w-max -translate-x-1/2 ${isShowingSuccess || isShowingFailure ? 'translate-y-16' : 'translate-y-0'} transform-gpu bg-white p-2 transition-transform duration-300 ease-out`}
+          className={`fixed left-1/2 top-5 w-4/5 max-w-max -translate-x-1/2 ${isShowingSuccess || isShowingFailure ? 'translate-y-16' : 'translate-y-0'} transform-gpu bg-white p-2 transition-transform duration-300 ease-out`}
         >
           <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-yellow-100 text-yellow-500">
             <HiTrophy className="size-5" />
@@ -72,9 +73,10 @@ const SubmissionToast = ({
             ? '-translate-y-36'
             : '-translate-y-20'
         }
+        className="fixed top-0 w-screen"
       >
         <Toast
-          className={`fixed left-1/2 top-5 w-max -translate-x-1/2 ${isShowingSuccess || isShowingFailure ? 'translate-y-16' : 'translate-y-0'} transform-gpu bg-white p-2 transition-transform duration-300 ease-out`}
+          className={`fixed left-1/2 top-5 w-4/5 max-w-max -translate-x-1/2 ${isShowingSuccess || isShowingFailure ? 'translate-y-16' : 'translate-y-0'} transform-gpu bg-white p-2 transition-transform duration-300 ease-out`}
         >
           <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-yellow-100 text-yellow-500">
             <HiMiniBolt className="size-5" />
@@ -96,8 +98,9 @@ const SubmissionToast = ({
         leave="transition-transform transform-gpu duration-300 ease-out"
         leaveFrom="translate-y-0"
         leaveTo="-translate-y-20"
+        className="fixed top-0 w-screen"
       >
-        <Toast className="fixed left-1/2 top-5 w-max -translate-x-1/2 bg-white p-2">
+        <Toast className="fixed left-1/2 top-5 w-4/5 max-w-max -translate-x-1/2 bg-white p-2">
           <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500">
             <HiCheck className="size-5" />
           </div>
@@ -121,8 +124,9 @@ const SubmissionToast = ({
         leave="transition-transform transform-gpu duration-300 ease-out"
         leaveFrom="translate-y-0"
         leaveTo="-translate-y-20"
+        className="fixed top-0 w-screen"
       >
-        <Toast className="fixed left-1/2 top-5 w-max -translate-x-1/2 bg-white p-2">
+        <Toast className="fixed left-1/2 top-5 w-4/5 max-w-max -translate-x-1/2 bg-white p-2">
           <div className="inline-flex size-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500">
             <HiXMark className="size-5" />
           </div>
